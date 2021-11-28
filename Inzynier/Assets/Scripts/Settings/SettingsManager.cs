a18d2845c0dd9d78c594ca0b11247f9ec3fa85bd
Kamsinox <92575198+Kamsinox@users.noreply.github.com>
2021-11-28 18:00:48 +0100

    Setting + Teoria
    Dodanie więcej opcji w ustawieniach i teorię do poczytania

--- Inzynier/Assets/Scripts/Settings/SettingsManager.cs
+++ Inzynier/Assets/Scripts/Settings/SettingsManager.cs
@@ -1,7 +1,10 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.Audio;
 using TMPro;
+using System;
+using UnityEngine.Audio;
+
 
 public class SettingsManager : MonoBehaviour
 {
@@ -12,25 +15,83 @@ public class SettingsManager : MonoBehaviour
 
     public AudioMixer audioMixer;
 
+    Resolution[] resolutions;
+    public TMPro.TMP_Dropdown resDropdown;
+
+    public TMPro.TMP_Dropdown qualDropdown;
+
+    public Toggle fullscreenToggle;
+
     public void SetVolume (float volume)
     {
-        audioMixer.SetFloat("volume", volume);
+        audioMixer.SetFloat("volume", Mathf.Log10(volume)*20);
+
         PlayerPrefs.SetFloat("volume", volume);
-        volumeVal.text = PlayerPrefs.GetFloat("volume").ToString();
+        volumeVal.text = Math.Round(PlayerPrefs.GetFloat("volume")*100,0).ToString();
         PlayerPrefs.Save();
     } 
 
     void Start()
     {
+        if(PlayerPrefs.GetInt("fullscreen") == 1) fullscreenToggle.isOn = true;
+        else fullscreenToggle.isOn = false;
+        startResolutions();
         setSlider();
+        resDropdown.value = PlayerPrefs.GetInt("resolution");
         slider.value = PlayerPrefs.GetFloat("volume", sliderValue);
+        qualDropdown.value = PlayerPrefs.GetInt("quality");
+    }
+
+    private void startResolutions()
+    {
+        resolutions = Screen.resolutions;
+        resDropdown.ClearOptions();
+        List<string> options = new List<string>();
+
+        int currentResIndex = 0;
+        for(int i=0 ;i<resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+
+            // if(resolutions[i].width == Screen.width && resolutions[i].height == Screen.height)
+            // {
+            //     currentResIndex = i;
+            // }
+        }
+
+        resDropdown.AddOptions(options);
+        resDropdown.value = currentResIndex;
+        resDropdown.RefreshShownValue();
+    }
+
+    public void setResolution(int resIndex)
+    {
+        Resolution resolution = resolutions[resIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+        PlayerPrefs.SetInt("resolution",resIndex);
+        PlayerPrefs.Save();
     }
 
     private void setSlider()
     {
         sliderValue = PlayerPrefs.GetFloat("volume");
-        volumeVal.text = sliderValue.ToString();
+        volumeVal.text = Math.Round((sliderValue*100),0).ToString();
         PlayerPrefs.SetFloat("volume",sliderValue);
+        PlayerPrefs.Save();
+    }
+
+    public void setQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+        PlayerPrefs.SetInt("quality", qualityIndex);
+        PlayerPrefs.Save();
+    }
+
+    public void setFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+        PlayerPrefs.SetInt("fullscreen",(isFullscreen == true) ? 1 : 0);
         PlayerPrefs.Save();
     }
 
